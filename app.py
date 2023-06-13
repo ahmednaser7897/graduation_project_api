c0f6baa0ac093d81f8b2f1ea35ad9e745ae1d90d
ahmed naser <ahmednaser7897>
2023-06-13 03:32:51 +0300

    add index

--- app.py
+++ app.py
@@ -4,23 +4,23 @@ from test import *
 
 
 keras.utils.get_custom_objects()['weighted_binary_crossentropy'] = weighted_binary_crossentropy
-
-def predict_old_model(mainImage):
-    print("predict_old_model")
-    # Generate predictions on test data
-    predictions = old_model.predict(mainImage)
-    print("3 : change_threshold")
-    predictions = change_threshold(predictions)
-    print("4 : predictions.shape")
-    print(predictions.shape)
-    image = get_img_from_array(predictions[0, :, :, 0])
-    print("5 : image type")
-    print(image)
-    # image.save("geeks.png")
-    image = im_2_b64(image)
-    print("6 : im_2_b64 ")
-    print(image)
-    return image
+#
+# def predict_old_model(mainImage):
+#     print("predict_old_model")
+#     # Generate predictions on test data
+#     predictions = old_model.predict(mainImage)
+#     print("3 : change_threshold")
+#     predictions = change_threshold(predictions)
+#     print("4 : predictions.shape")
+#     print(predictions.shape)
+#     image = get_img_from_array(predictions[0, :, :, 0])
+#     print("5 : image type")
+#     print(image)
+#     # image.save("geeks.png")
+#     image = im_2_b64(image)
+#     print("6 : im_2_b64 ")
+#     print(image)
+#     return image
 
 def predict_new_model(mainImage):
     print("predict_new_model")
@@ -46,7 +46,7 @@ def predict_new_model(mainImage):
 new_model = load_new_model()
 #old_model = load_old_model()
 
-app = Flask(__name__)  
+app = Flask(__name__)
 
 
 @app.route('/predictApi', methods=["POST"])
@@ -71,6 +71,10 @@ def api2():
         #return jsonify({'new_model_predictions': new_model_predictions.decode("utf-8"),'old_model_predictions': old_model_predictions.decode("utf-8")})
     except:
        return jsonify({'Error': 'Error occur'})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
